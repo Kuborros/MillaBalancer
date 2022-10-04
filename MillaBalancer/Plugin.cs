afbadefde205629d3733d82a27c98b9652ac7397
Kuborros <kubo@catbat.rocks>
2022-10-04 22:37:30 +0200

    Renamed the mod

--- MillaBalancer/Plugin.cs
+++ MillaBalancer/Plugin.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 
 namespace MillaBalancer
 {
-    [BepInPlugin("com.kuborro.plugins.fp2.millabal", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInPlugin("com.kuborro.plugins.fp2.millacube", "Milla cube range editor", "1.0.0")]
     [BepInProcess("FP2.exe")]
     public class Plugin : BaseUnityPlugin
     {
         public static ConfigEntry<float> configRange;
         private void Awake()
         {
-            configRange = Config.Bind("General", "Range",(float) -50.0, "Set Milla's cube/projectile range. Value must be negative float. 0 = No range, -100 = Wraps whole screen vertically twice.");
+            configRange = Config.Bind("General", "Range",(float) -50.0, "Set Milla's cube/projectile range. Value must be negative float. 0 = No range, -10 = Default, -50 = Basically infinite.");
 
             if (configRange.Value > (float) 0.0)
             {
                 configRange.Value = (float) -50.0;
             }
                 //HarmonyFileLog.Enabled = true;
-                var harmony = new Harmony("com.kuborro.plugins.fp2.millabal");
+                var harmony = new Harmony("com.kuborro.plugins.fp2.millacube");
                 harmony.PatchAll(typeof(Patch));
         }
     }
