a88252d858441a6cca88169e7c5cf648d4ce30fa
Kuborros <kubo@catbat.rocks>
2022-10-05 23:52:55 +0200

    Added modifier for amount of cubes shot

--- MillaBalancer/Plugin.cs
+++ MillaBalancer/Plugin.cs
@@ -1,18 +1,21 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
+using MillaBalancer;
 using UnityEngine;
 
 namespace MillaBalancer
 {
-    [BepInPlugin("com.kuborro.plugins.fp2.millacube", "Milla cube range editor", "1.0.0")]
+    [BepInPlugin("com.kuborro.plugins.fp2.millacube", "Milla cube range editor", "1.1.0")]
     [BepInProcess("FP2.exe")]
     public class Plugin : BaseUnityPlugin
     {
         public static ConfigEntry<float> configRange;
+        public static ConfigEntry<float> configProjectiles;
         private void Awake()
         {
             configRange = Config.Bind("General", "Range",(float) -50.0, "Set Milla's cube/projectile range. Value must be negative float. 0 = No range, -10 = Default, -50 = Basically infinite.");
+            configProjectiles = Config.Bind("General", "Cubes fired per cube", (float) 500.0, "Set how many cubes can Milla fire per one follower cube . Value must be a positive float. 0 = No cubes, 100 = Default, 500 = 5x the shots.");
 
             if (configRange.Value > (float) 0.0)
             {
@@ -21,6 +24,7 @@ namespace MillaBalancer
                 //HarmonyFileLog.Enabled = true;
                 var harmony = new Harmony("com.kuborro.plugins.fp2.millacube");
                 harmony.PatchAll(typeof(Patch));
+                harmony.PatchAll(typeof(Patch2));
         }
     }
 
@@ -35,6 +39,19 @@ namespace MillaBalancer
             {
                 ___explodeTimer = Plugin.configRange.Value;
             }
+        }
+    }
+}
+
+class Patch2
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(FPPlayer), nameof(FPPlayer.Action_MillaCubeSpawn), MethodType.Normal)]
+    [HarmonyPatch(typeof(FPPlayer), nameof(FPPlayer.Action_MillaMultiCube), MethodType.Normal)]
+    static void Postfix(ref float ___millaCubeEnergy)
+    {
+        {
+            ___millaCubeEnergy = Plugin.configProjectiles.Value;
         }
     }
 }
